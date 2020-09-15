d05d75362ee0c08c03fe6cf75b005003ce557c11
sburton30693 <sburton30693@gmail.com>
2020-09-15 09:36:38 -0600

    Added comments to the chapter 2.1 program

--- chpt2.1.py
+++ chpt2.1.py
@@ -1,8 +1,14 @@
+# Spencer Burton
+# 9/15/2020
+# Chapter 2 Examples
+
+# Number types
 x = 3
 print(type(x))
 x = x + 0.8
 print(type(x))
 
+# String and Boolean types
 name = "Spencer"
 print(type(name))
 awake = True
