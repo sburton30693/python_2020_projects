b9eaf864115a3c8a0c86e7704f35d1ee65e9d6b7
sburton30693 <sburton30693@gmail.com>
2020-09-17 09:07:50 -0600

    Added chapter 2.2 file, and renamed chapter 2.1 file

---
+++ ch2.2.py
@@ -0,0 +1,16 @@
+# Spencer Burton
+# 9/17/2020
+# Chapter 2.2 data types examples
+
+number = 10
+print(type(number))
+score = 10000
+number2 = 7.0
+answer = number + number2
+print(type(answer))
+print(answer)
+
+print(2 + 5 * 7 / 8 + 3 * ( 8 + 7 * 4))
+
+print("score: " + str(score + 100))
+print("score:", score + 100)
