e67d6553fdb9ffa0befd144a5e790534cc58f014
sburton30693 <sburton30693@gmail.com>
2020-10-01 09:28:03 -0600

    Finished number guessing game and added chapter 4 prize winner file

--- ch4_number.py
+++ ch4_number.py
@@ -6,23 +6,94 @@ import random
 
 # Choose a random number
 the_number = random.randint(1, 10)
+win = False
+count = 0
 
 # Display rules to user
 print("\tWelcome to 'Guess My Number'!")
 print("I'm thinking of a number between 1 and 10.")
 print("Try to guess it in 3 attempts.\n")
 
+
 # Get user input for guess
 user_guess = int(input("Guess a number between 1 and 10: "))
 
 # Check guess against the number
-if user_guess == the_number :
-    pass
+if not win :
+    if user_guess == the_number :
+        print("Correct!")
+        win = True
 
-elif user_guess > the_number :
-    pass
-    print("Nope, try guessing lower")
+    elif user_guess > the_number :
+        print("Nope, guess lower")
 
+    else :
+        print("Nope, guess higher")
+
+
+# Second Guess
+
+if not win :
+    user_guess = int(input("Guess a number between 1 and 10: "))
+
+    if user_guess == the_number :
+        print("Correct!")
+        win = True
+
+    elif user_guess > the_number :
+        print("Nope, guess lower")
+
+    else :
+        print("Nope, guess higher")
+
+if not win :
+    if count > 0 :
+        user_guess = int(input("Guess a number between 1 and 10: "))
+
+        if user_guess == the_number :
+            print("Correct!")
+            win = True
+
+        elif user_guess > the_number :
+            print("Nope, guess lower")
+
+        else :
+            print("Nope, guess higher")
+
+if not win :
+    if count > 0 :
+        user_guess = int(input("Guess a number between 1 and 10: "))
+
+        if user_guess == the_number :
+            print("Correct!")
+            win = True
+
+        elif user_guess > the_number :
+            print("Nope, guess lower")
+
+        else :
+            print("Nope, guess higher")
+
+
+# Third Guess
+
+if not win :
+    user_guess = int(input("Guess a number between 1 and 10: "))
+
+    if user_guess == the_number :
+        print("Correct!")
+        win = True
+
+    elif user_guess > the_number :
+        print("Nope, you lost")
+        print("The number was", the_number)
+
+    else :
+        print("Nope, you lost")
+        print("The number was", the_number)
+
+
+if win :
+    print("Great Job")
 else :
-    pass
-    print("Nope, try guessing higher")
+    print("Better luck next time")
---
+++ ch4_prize.py
@@ -0,0 +1,19 @@
+# Spencer Burton
+# Chapter 4 Prize Winner
+
+# ticket numbers
+number1 = 3
+number2 = 5
+number3 = 2
+number4 = 4
+number5 = 8
+number6 = 0
+
+# complete the rest of the program code below
+
+if (number1 > number6) and \
+   (number2 * number3 > 20) and \
+   (number4 - (number5 * number1) < 0) :
+  print("Winner!")
+else :
+  print("Thanks for your contributions to the math club")
