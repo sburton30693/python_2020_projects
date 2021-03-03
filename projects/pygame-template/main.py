c0dc183239788205ef661f3a8e5a6c27317edc6e
sburton30693 <sburton30693@gmail.com>
2021-03-03 10:09:45 -0700

    Added some stuff

--- projects/pygame-template/main.py
+++ projects/pygame-template/main.py
@@ -4,7 +4,7 @@
 # Imports
 import pygame
 import random
-import math
+import os
 
 
 # Constants
@@ -78,6 +78,7 @@ class Npc(pygame.sprite.Sprite):
             self.image.fill(PURPLE)
             self.speed_num += self.acc
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
@@ -87,10 +88,48 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.speed_num = 5
         self.speed = [0, 0]
+        self.key_pressed = False
+        self.mouse_button_held = False
 
     def update(self):
+        # self.speed[0] = 0
+        # self.speed[1] = 0
+
+        if self.mouse_button_held:
+            self.rect.center = pygame.mouse.get_pos()
+
+        keystate = pygame.key.get_pressed()
+
+        # if keystate[pygame.K_LEFT]:
+        #     self.speed[0] = -5
+        # if keystate[pygame.K_RIGHT]:
+        #     self.speed[0] = 5
+        # if keystate[pygame.K_UP]:
+        #     self.speed[1] = -5
+        # if keystate[pygame.K_DOWN]:
+        #     self.speed[1] = 5
+
         self.rect.x += self.speed[0] * self.speed_num
         self.rect.y += self.speed[1] * self.speed_num
+
+        # Bind player to screen
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+
+
+def spawn_new_player(x, y, all_sprites, players_group):
+    new_player = Player()
+    new_player.rect.center = (x, y)
+    new_player.speed[0] = random.randint(-5, 5)
+    new_player.speed[1] = random.randint(-5, 5)
+    all_sprites.add(new_player)
+    players_group.add(new_player)
 
 
 # Main Function
@@ -101,7 +140,6 @@ def main():
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption(TITLE)
-
     clock = pygame.time.Clock()
 
     # Sprite Groups
@@ -128,32 +166,15 @@ def main():
         # Process Input
         for event in pygame.event.get():
 
-            # Basic grid movement
+            if event.type == pygame.MOUSEBUTTONDOWN and player.rect.collidepoint(pygame.mouse.get_pos()):
+                player.mouse_button_held = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                player.mouse_button_held = False
+                spawn_new_player(player.rect.centerx, player.rect.centery, all_sprites, players_group)
+
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.rect.x -= 50
-                if event.key == pygame.K_RIGHT:
-                    player.rect.x += 50
-                if event.key == pygame.K_UP:
-                    player.rect.y -= 50
-                if event.key == pygame.K_DOWN:
-                    player.rect.y += 50
-
-                # Flow movement
-                #if event.key == pygame.K_LEFT:
-                #    player.speed[0] = -1
-                #if event.key == pygame.K_RIGHT:
-                #    player.speed[0] = 1
-                #if event.key == pygame.K_UP:
-                #    player.speed[1] = -1
-                #if event.key == pygame.K_DOWN:
-                #    player.speed[1] = 1
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    player.speed[0] = 0
-                if event.key == pygame.K_UP or event.key == pygame.K_LEFT:
-                    player.speed[1] = 0
+                if event.key == pygame.K_ESCAPE:
+                    running = False
 
             # Check for closing window
             if event.type == pygame.QUIT:
